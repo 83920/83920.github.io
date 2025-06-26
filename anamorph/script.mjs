f18a65365b58ab5ad53d02dd75f69698b32acbc3
83920 <unique.morphism@gmail.com>
2025-06-26 19:32:42 +0200

    Update script.mjs

--- anamorph/script.mjs
+++ anamorph/script.mjs
@@ -1,14 +1,4 @@
 
-// I guess I'm most leaning towards:
-// Define a 'super' sdf. Inside that one, sample N points (with a fixed seed so it's the
-// same for each pixel). First render it without the reflection in which case basically smooth
-// them together (metaballs). (If you sample them from a grid instead of random then there's fewer
-// neighbours to look for intersections. Or skip it since forming the union/smooth union basically
-// boils down to looping over all particles, and that's about it. It might be plenty fast for
-// even a high resolution.) Anyway then you place them down in the reflection. And since
-// that part is closed form etc and you get a location for each point from that, then you can just
-// render the points as sdfs at that location.
-
 import * as gpu from './gpu.mjs';
 const gl = document.getElementById('c').getContext('webgl2', { powerPreference: 'high-performance' });
 gl.disable(gl.DEPTH_TEST); gl.disable(gl.STENCIL_TEST);
